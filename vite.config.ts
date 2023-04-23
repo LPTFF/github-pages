3117d73e13e9abcf39acf422f25732695eebb08d
LPTFF <lptffhh@gmail.com>
2023-04-23 19:29:17 +0800

    调整项目

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/github-pages-test/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/github-pages/' : './',
   plugins: [vue()],
   server: {
     cors: true,
