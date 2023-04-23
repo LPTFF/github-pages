696b7d5dd9660e07da2b0e72fadefc8aaa3ef2cd
LPTFF <lptffhh@gmail.com>
2023-04-23 19:16:13 +0800

    固定端口

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/github-pages-test/' : './',
   plugins: [vue()],
+  server: {
+    cors: true,
+    open: true,
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.99.223:3000',   //代理接口
+        changeOrigin: true
+      }
+    }
+  }
 });
